181f43a459ab5e2e0dda939360c2db7814236ac8
linoyodi-sketch <linoyodi@gmail.com>
2026-03-22 03:38:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,19 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// המפתח האישי שלך
+// המפתח שלך
 const ABLY_API_KEY = 'LQMvpw.UUJZZw:VsdRVinvYPA4OF48UcYBOU6klbIhEp-uzmJ3gKnxD2w';
 const realtime = new Ably.Realtime(ABLY_API_KEY);
 
-app.use(cors()); // מאפשר לאתר וורדפרס שלך לדבר עם השרת הזה
+app.use(cors());
 app.use(express.json());
 
-// דף בית קטן כדי לדעת שהשרת חי
 app.get('/', (req, res) => {
     res.send('Martef Server is Live and Ready!');
 });
 
-// מנגנון זיהוי: יוצר טוקן זמני לכל משתמש (רשום או אורח)
 app.get('/auth', (req, res) => {
     const clientId = req.query.userId || 'guest_' + Math.random().toString(36).substr(2, 9);
-    
-    // הרשאות: מותר לקרוא, לכתוב ולראות מי מחובר
     const capability = { "*": ["publish", "subscribe", "presence"] };
 
     realtime.auth.createTokenRequest({ clientId: clientId, capability: capability }, (err, tokenRequest) => {
@@ -33,5 +29,5 @@ app.get('/auth', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(Server running on port ${port});
+    console.log(`Server running on port ${port}`);
 });
